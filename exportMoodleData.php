b89b85413f9103c356b5cf3d3836e5ba6f3caf9b
alice-d <alice.dourado@tecnico.ulisboa.pt>
2019-06-03 13:30:40 +0100

    export moodle remove html tagss

--- exportMoodleData.php
+++ exportMoodleData.php
@@ -12,6 +12,56 @@ $users = Core::$systemDB->selectMultiple("mdl_user");
 $userMap = array_combine(array_column($users, "id"), $users);
 
 $folder="moodleExport";
+function computePost($post,$userMap,$discussion,$forum,$course, $discussionFolder){
+    //echo "Looking at post " . $post['name'] . "<br>";
+
+    $post["username"]=$userMap[$post["userid"]]["username"];
+    $post["userFirstLastName"]=utf8_encode($userMap[$post["userid"]]['firstname'] .' '.$userMap[$post["userid"]]['lastname']);
+    unset($post['userid']);
+
+    $post["discussion"]=utf8_encode($discussion['name']);
+    $post["forum"]=utf8_encode($forum['name']);
+    
+    
+    //file_put_contents($discussionFolder . '/'. $post['id'].".html",$post["message"]);
+    //file_put_contents($discussionFolder . '/'. $post['id'].".txt",$post["message"]);
+    
+    //Removing some html tags to make a "cleaner" output
+    $post["message"]=(preg_replace('#<[/]?p>#','',$post["message"]));
+    $post["message"]=(preg_replace('#<[/]?span[^>]*>#','',$post["message"]));
+    $post["message"]=(preg_replace('#<[/]?strong[^>]*>#','',$post["message"]));
+    $post["message"]=(preg_replace('#<[/]?ul[^>]*>#','',$post["message"]));
+    $post["message"]=(preg_replace('#<[/]?li[^>]*>#','',$post["message"]));
+    $post["message"]=(preg_replace('#<[/]?em>#','',$post["message"]));
+    $post["message"]=(preg_replace('#<br[^>]*>#','',$post["message"]));
+    $post["message"]=(preg_replace('#<[/]?pre>#','',$post["message"]));
+    //$post["message"]=(preg_replace('#\s:[pP]\s#','',$post["message"]));
+    //$post["message"]=(preg_replace('#\.{3,4}#','',$post["message"]));
+    //$post["message"]=(preg_replace('#;#','',$post["message"]));
+    
+    $post["message"]=(preg_replace('/<img[^>]*>/',' IMAGE ',$post["message"]));
+    //$post["message"]=(preg_replace('#<a[^>]*href=\"([^> \"]*)\"[^>]*>[^<]*</a>#','$1',$post["message"]));
+    $post["message"]=(preg_replace('#<a[^>]*>[^<]*</a>#',' WEBLINK ',$post["message"]));
+    //file_put_contents($discussionFolder . '/'. $post['id']."After.txt",$post["message"]);
+     
+    $post["message"] = utf8_encode($post["message"]);
+    
+    //$post["message"]=(preg_replace('#\r\n#','',$post["message"]));/
+    
+    $post["course"]=utf8_encode($course["shortname"]);
+
+    $content = json_encode($post);
+
+    $error = json_last_error_msg();
+    if (json_last_error_msg()!="No error"){
+        print_r($error . "<br> On json encoding of the following info: <br>");
+        print_r($post);
+        echo "<br>";
+    }
+    file_put_contents($discussionFolder . '/'. $post['id'].".json",$content);
+    //echo "added file to ". $discussionFolder . "<br>";
+}
+
 if (!file_exists($folder))
     mkdir($folder);
 foreach($courses as $course){
@@ -46,34 +96,18 @@ foreach($courses as $course){
                     continue;
                 }
                 $post = Core::$systemDB->select("mdl_forum_posts","message,id,userid,parent,created",["id"=>$post['id']]);
-                //echo "Looking at post " . $post['name'] . "<br>";
-
-                $post["username"]=$userMap[$post["userid"]]["username"];
-                $post["userFirstLastName"]=utf8_encode($userMap[$post["userid"]]['firstname'] .' '.$userMap[$post["userid"]]['lastname']);
-                unset($post['userid']);
-                
-                $post["discussion"]=utf8_encode($discussion['name']);
-                $post["forum"]=utf8_encode($forum['name']);
-                $post["message"]=utf8_encode($post["message"]);
-                $post["course"]=utf8_encode($course["shortname"]);
-
-                
-                $content = json_encode($post);
-
-                $error = json_last_error_msg();
-                if (json_last_error_msg()!="No error"){
-                    print_r($error . "<br> On json encoding of the following info: <br>");
-                    print_r($post);
-                    echo "<br>";
-                }
-                file_put_contents($discussionFolder . '/'. $post['id'].".json",$content);
-                //echo "added file to ". $discussionFolder . "<br>";
+                computePost($post, $userMap, $discussion, $forum, $course, $discussionFolder);
             }
         }
     }
-    $peerforums = Core::$systemDB->selectMultiple("mdl_peerforum","id,name",["course"=>$course['id']]);
+    try {
+        $peerforums = Core::$systemDB->selectMultiple("mdl_peerforum","id,name",["course"=>$course['id']]);
+    } catch (Exception $ex) {
+        $peerforums=[];
+    }
+    
     foreach($peerforums as $forum){
-        echo "Looking at peerforum " . $forum['name'] . "<br>";
+        //echo "Looking at peerforum " . $forum['name'] . "<br>";
         $forum["name"]= str_replace('/', '-', $forum["name"]);
         $forumFolder=$courseFolder .'/'. $forum['id'].'-'.preg_replace("/[^a-zA-Z0-9_ ]/","",$forum["name"]);
         if (!file_exists($forumFolder))
@@ -81,7 +115,7 @@ foreach($courses as $course){
         
         $discussions = Core::$systemDB->selectMultiple("mdl_peerforum_discussions","id,name",["peerforum"=>$forum['id']]);
         foreach($discussions as $discussion){
-            echo "Looking at discussion " . $discussion['name'] . "<br>";
+            //echo "Looking at discussion " . $discussion['name'] . "<br>";
             $discussion["name"]= str_replace('/', '-', $discussion["name"]);
             $discussionFolder=$forumFolder .'/'. $discussion['id'].'-'.preg_replace("/[^a-zA-Z0-9_ ]/","",$discussion["name"]);
             if (!file_exists($discussionFolder))
@@ -91,31 +125,11 @@ foreach($courses as $course){
             //message,id,userid,parent,created
             foreach($posts as $post){
                 $post = Core::$systemDB->select("mdl_peerforum_posts","message,id,userid,parent,created",["id"=>$post['id']]);
-                //echo "Looking at post " . $post['name'] . "<br>";
-
-                $post["username"]=$userMap[$post["userid"]]["username"];
-                $post["userFirstLastName"]=utf8_encode($userMap[$post["userid"]]['firstname'] .' '.$userMap[$post["userid"]]['lastname']);
-                unset($post['userid']);
-                
-                $post["discussion"]=utf8_encode($discussion['name']);
-                $post["forum"]=utf8_encode($forum['name']);
-                $post["message"]=utf8_encode($post["message"]);
-                $post["course"]=utf8_encode($course["shortname"]);
-
-                
-                $content = json_encode($post);
-
-                $error = json_last_error_msg();
-                if (json_last_error_msg()!="No error"){
-                    print_r($error . "<br> On json encoding of the following info: <br>");
-                    print_r($post);
-                    echo "<br>";
-                }
-                file_put_contents($discussionFolder . '/'. $post['id'].".json",$content);
-                //echo "added file to ". $discussionFolder . "<br>";
+                computePost($post, $userMap, $discussion, $forum, $course, $discussionFolder);
             }
         }
     }
+    //break;//Todo delete
 }
 echo "Done";
 ?>
