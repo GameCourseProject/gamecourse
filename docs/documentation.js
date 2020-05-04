f0bb5784b41bcf255d620b394c2dbe488478109b
patsilvarte <ptrc.silva1@live.com.pt>
2020-05-04 18:41:52 +0100

    Add functionality of menu and some other fixes from that

--- docs/documentation.js
+++ docs/documentation.js
@@ -1,5 +1,4 @@
-prevTab = "tab-views";
-prevSection = "views";
+//functions to interact on the documentation pages
 
 function showContent(selector, name) {
     //define the selected tab
@@ -15,7 +14,10 @@ function showContent(selector, name) {
     prevSection = name;
 } 
 
-$(document).ready(function(){
+function addClickEventsViews(){
+    prevTab = "tab-views";
+    prevSection = "views";
+    prevMenu = "#menu-views";
 
     $("#tab-views").on('click', function (){ 
         showContent( "tab-views","views");}
@@ -28,6 +30,43 @@ $(document).ready(function(){
     );
     $("#tab-config").on('click', function (){ 
         showContent("tab-config", "part-configuration");}
+    );
+}
+
+function changeMenu(new_menu){
+    menu = $(new_menu);
+    page = $(".page");
+
+    //focus right menu option
+    $(prevMenu).children().first().removeClass("focused");
+    menu.children().first().addClass("focused");
+
+    //reveal right partial
+    switch (new_menu){
+        case "#menu-views":
+            console.log("views menu");
+            php_div = 'pages/plugins/views.html';
+            page.load(php_div, function(){
+                addClickEventsViews();
+            });
+            break;
+        case "#menu-module":
+            console.log("module menu");
+            php_div = 'pages/modules.html';
+            page.load(php_div);
+            break;
+    }
+    prevMenu = new_menu;
+}
+
+$(document).ready(function(){
+    addClickEventsViews();
+    
+    $("#menu-views").on('click', function (){ 
+        changeMenu("#menu-views");}
+    );
+    $("#menu-module").on('click', function (){ 
+        changeMenu("#menu-module");}
     );
 
 
--- docs/index.php
+++ docs/index.php
@@ -7,7 +7,7 @@
     <title>GameCourse Documentation</title>
     <link rel="stylesheet" href="/gamecourse/css/navbar.css"/>
     <link rel="stylesheet" href="./css/docs.css"/>
-    <link rel="stylesheet" href="./css/geral.css"/>
+    <link rel="stylesheet" href="/gamecourse/css/geral.css"/>
     <script src="/gamecourse/js/angular.min.js"></script>
     <script src="/gamecourse/js/jquery.min.js"></script>
     <script type="text/javascript" src="documentation.js"></script>
@@ -16,16 +16,16 @@
     <div class="navbar">  
         <div class="logo">logo</div>
         <ul class="menu documentation">
-            <li><a href=".">Docs</a></li>
-            <li><a href="./functions">Functions</a></li>
-            <li><a href="./pages/modules.html">Modules</a></li>
+            <li id="menu-views"><a href="javascript:void(0);" class="focused">Views</a></li>
+            <li id="menu-functions"><a href="./functions">Functions</a></li>
+            <li id="menu-module"><a href="javascript:void(0);">Modules</a></li>
         </ul>
     </div>
 
     <div class="page">
         <?php include'pages/plugins/views.html'; ?>
-        <!--?php include'pages/plugins/charts.html'; ?>
-        <!--?php include'pages/modules.html'; ?-->
+        <!--?php include'pages/plugins/charts.html'; ?-->  <!-- Not added -->
+        <!--?php include'pages/modules.html'; ?-->         <!-- Added later on js -->
     </div>
 </body>
 </html>
