66cbb1db3c9bfcc37738239f899a59092a14a2d8
Diana Lopes <dianamlopes22@gmail.com>
2020-06-26 00:07:29 +0100

    Delete script for all plugins

---
+++ modules/plugin/delete.sql
@@ -0,0 +1,8 @@
+drop table if exists attendance;
+drop table if exists config_class_check;
+drop table if exists config_fenix;
+drop table if exists config_google_sheets;
+drop table if exists config_moodle;
+drop table if exists moodle_logs;
+drop table if exists moodle_votes;
+drop table if exists moodle_quiz_grades;
