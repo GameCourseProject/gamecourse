dac36582d9427d1cb278c46c36a33683309f0afe
patsilvarte <ptrc.silva1@live.com.pt>
2020-10-23 02:29:58 +0100

    create modal for error messages from API

--- js/modals.js
+++ js/modals.js
@@ -11,3 +11,20 @@ function closeModal(element){
     modal = $(id);
     modal.hide();
 }
+
+function removeModal(element){
+    id = element.getAttribute('value');
+    modal = $(id);
+    modal.remove();
+}
+
+function giveError(message){
+    modal = $("<div class='modal' id='error-api-box'></div>");
+    verification = $("<div class='verification modal_content'></div>");
+    verification.append( $('<button class="close_btn icon" value="#error-api-box" onclick="removeModal(this)"></button>'));
+    verification.append( $('<div class="error_warning warning">'+ message +'</div>'));
+    verification.append( $('<div class="confirmation_btns"><button value="#error-api-box" onclick="removeModal(this)">Confirm</button></div>'))
+    modal.append(verification);
+    $("body").append(modal);
+    modal.show();
+}
