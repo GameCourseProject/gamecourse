ae4c5e90c663b66ab3c41244a4b51d3f5be1d1af
AnaRitaGoncalves <ana.goncalves166@gmail.com>
2021-03-24 23:30:11 +0000

    check if qr is valid & delete config_qr table

--- modules/qr/create.sql
+++ modules/qr/create.sql
@@ -7,19 +7,12 @@ create table qr_code(
 	foreign key (course) references course(id) on delete cascade
 );
 create table qr_error(
-	studentNumber int unsigned not null,
+	user int unsigned not null,
 	course  int unsigned not null,
-	major varchar(8),
 	ip varchar(50),
 	qrkey varchar(50), 
 	msg varchar(500),
-	date timestamp default CURRENT_TIMESTAMP
+	date timestamp default CURRENT_TIMESTAMP,
+	foreign key (course) references course(id) on delete cascade,
+	foreign key (user) references game_course_user(id) on delete cascade
 );
-create table config_qr(
-	id 		int unsigned auto_increment primary key,
-	course  int unsigned not null,
-	isEnabled boolean,
-	periodicityNumber int,
-	periodicityTime varchar(25),
-	foreign key (course) references course(id) on delete cascade
-)
--- modules/qr/delete.sql
+++ modules/qr/delete.sql
@@ -1,3 +1,2 @@
 drop table if exists qr_code;
 drop table if exists qr_error;
-drop table if exists config_qr;
