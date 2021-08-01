e35c8f2d69d9588027ad11c3beb7eaa6322cc556
joanasesinando@tecnico.ulisboa.pt <joanasesinando@tecnico.ulisboa.pt>
2021-08-01 17:42:17 +0100

    Started building API

--- backend/index.php
+++ backend/index.php
@@ -1,1 +1,18 @@
 <?php
+// The job of index.php is to determine what file to load
+
+$action = isset($_GET['action']) ? $_GET['action'] : null;
+
+if (!$action) {
+    die('Error: no action found');
+}
+
+switch ($action) {
+    case 'login':
+        require('API/login.php');
+        break;
+
+    default:
+        echo 'Error: action \'' . $_GET['action'] . '\' not found';
+        break;
+}
