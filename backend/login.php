74f1c93e81adcce9274fac86ec91813ddbcfdf8c
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2022-03-07 14:27:48 +0000

    Removed unsued import

--- backend/login.php
+++ backend/login.php
@@ -8,7 +8,6 @@ include 'classes/ClassLoader.class.php';
 
 use GameCourse\Core;
 use GameCourse\ModuleLoader;
-use GameCourse\Views\Dictionary;
 
 if (array_key_exists("logout", $_GET)) {
     Core::logout();
