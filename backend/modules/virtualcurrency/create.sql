9df23d12a63cac6b187fb705259f8b0e1f984bbc
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2022-05-04 17:02:08 +0100

    Updated virtual currency table

--- backend/modules/virtualcurrency/create.sql
+++ backend/modules/virtualcurrency/create.sql
@@ -13,6 +13,7 @@ create table user_wallet(
     course int unsigned not null,
     user int unsigned not null,
     tokens int not null,
+    exchanged boolean NOT NULL DEFAULT FALSE
     primary key(user, course),
     foreign key(course) references course(id) on delete cascade,
     foreign key(user) references game_course_user(id) on delete cascade
