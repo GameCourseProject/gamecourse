5e48322faeed19f07bc8eac13a6a99f4bf18de08
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2022-08-19 12:17:42 +0100

    Rule System: started frontend

--- frontend/src/app/_views/restricted/courses/course/settings/rules/rules.component.ts
+++ frontend/src/app/_views/restricted/courses/course/settings/rules/rules.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Reduce} from "../../../../../../_utils/lists/reduce";
+import {Course} from "../../../../../../_domain/courses/course";
+import {ApiHttpService} from "../../../../../../_services/api/api-http.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-rules',
@@ -7,11 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RulesComponent implements OnInit {
 
-  loading: boolean;
+  loading: boolean = true;
 
-  constructor() { }
+  course: Course;
+
+  reduce = new Reduce();
+  searchQuery: string;
+
+  constructor(
+    private api: ApiHttpService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.parent.params.subscribe(async params => {
+      const courseID = parseInt(params.id);
+      await this.getCourse(courseID);
+      this.loading = false;
+    });
   }
 
+
+  /*** --------------------------------------------- ***/
+  /*** -------------------- Init ------------------- ***/
+  /*** --------------------------------------------- ***/
+
+  async getCourse(courseID: number): Promise<void> {
+    this.course = await this.api.getCourseById(courseID).toPromise();
+  }
+
+
+  /*** --------------------------------------------- ***/
+  /*** -------------- Search & Filter -------------- ***/
+  /*** --------------------------------------------- ***/
+
+  reduceList(query?: string): void {
+    // this.reduce.search(this.modules, query); TODO
+  }
+
+
+  /*** --------------------------------------------- ***/
+  /*** ------------------ Sections ----------------- ***/
+  /*** --------------------------------------------- ***/
+
+  // TODO
+
+
+  /*** --------------------------------------------- ***/
+  /*** -------------------- Tags ------------------- ***/
+  /*** --------------------------------------------- ***/
+
+  // TODO
 }
