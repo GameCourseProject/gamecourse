b29466ad9397946d200834c3fd1eee51df86608f
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2022-09-01 01:25:04 +0100

    Leaderboard & Profile modules refactor

---
+++ api/modules/Leaderboard/Leaderboard.php
@@ -0,0 +1,60 @@
+<?php
+namespace GameCourse\Module\Leaderboard;
+
+use GameCourse\Course\Course;
+use GameCourse\Module\Badges\Badges;
+use GameCourse\Module\DependencyMode;
+use GameCourse\Module\Module;
+use GameCourse\Module\ModuleType;
+use GameCourse\Module\XPLevels\XPLevels;
+
+/**
+ * This is the Leaderboard module, which serves as a compartimentalized
+ * plugin that adds functionality to the system.
+ */
+class Leaderboard extends Module
+{
+    public function __construct(?Course $course)
+    {
+        parent::__construct($course);
+        $this->id = self::ID;
+    }
+
+
+    /*** ----------------------------------------------- ***/
+    /*** ------------------ Metadata ------------------- ***/
+    /*** ----------------------------------------------- ***/
+
+    const ID = "Leaderboard";  // NOTE: must match the name of the class
+    const NAME = "Leaderboard";
+    const DESCRIPTION = "Provides different leaderboard templates with students' progress on the course.";
+    const TYPE = ModuleType::GAME_ELEMENT;
+
+    const VERSION = "2.2.0";                                     // Current module version
+    const PROJECT_VERSION = ["min" => "2.2", "max" => null];     // Min/max versions of project for module to work
+    const API_VERSION = ["min" => "2.2.0", "max" => null];       // Min/max versions of API for module to work
+    // NOTE: versions should be updated on code changes
+
+    const DEPENDENCIES = [
+        ["id" => XPLevels::ID, "minVersion" => "2.2.0", "maxVersion" => null, "mode" => DependencyMode::HARD],
+        ["id" => Badges::ID, "minVersion" => "2.2.0", "maxVersion" => null, "mode" => DependencyMode::SOFT]
+    ];
+    // NOTE: dependencies should be updated on code changes
+
+    const RESOURCES = [];
+
+
+    /*** ----------------------------------------------- ***/
+    /*** -------------------- Setup -------------------- ***/
+    /*** ----------------------------------------------- ***/
+
+    public function init()
+    {
+        $this->initTemplates();
+    }
+
+    public function disable()
+    {
+        $this->removeTemplates();
+    }
+}
---
+++ api/modules/Profile/Profile.php
@@ -0,0 +1,67 @@
+<?php
+namespace GameCourse\Module\Profile;
+
+use GameCourse\Course\Course;
+use GameCourse\Module\Awards\Awards;
+use GameCourse\Module\Badges\Badges;
+use GameCourse\Module\DependencyMode;
+use GameCourse\Module\Module;
+use GameCourse\Module\ModuleType;
+use GameCourse\Module\Skills\Skills;
+use GameCourse\Module\Streaks\Streaks;
+use GameCourse\Module\VirtualCurrency\VirtualCurrency;
+use GameCourse\Module\XPLevels\XPLevels;
+
+/**
+ * This is the Profile module, which serves as a compartimentalized
+ * plugin that adds functionality to the system.
+ */
+class Profile extends Module
+{
+    public function __construct(?Course $course)
+    {
+        parent::__construct($course);
+        $this->id = self::ID;
+    }
+
+
+    /*** ----------------------------------------------- ***/
+    /*** ------------------ Metadata ------------------- ***/
+    /*** ----------------------------------------------- ***/
+
+    const ID = "Profile";  // NOTE: must match the name of the class
+    const NAME = "Profile";
+    const DESCRIPTION = "Provides templates for a profile page where user statistics are shown.";
+    const TYPE = ModuleType::GAME_ELEMENT;
+
+    const VERSION = "2.2.0";                                     // Current module version
+    const PROJECT_VERSION = ["min" => "2.2", "max" => null];     // Min/max versions of project for module to work
+    const API_VERSION = ["min" => "2.2.0", "max" => null];       // Min/max versions of API for module to work
+    // NOTE: versions should be updated on code changes
+
+    const DEPENDENCIES = [
+        ["id" => XPLevels::ID, "minVersion" => "2.2.0", "maxVersion" => null, "mode" => DependencyMode::HARD],
+        ["id" => VirtualCurrency::ID, "minVersion" => "2.2.0", "maxVersion" => null, "mode" => DependencyMode::SOFT],
+        ["id" => Badges::ID, "minVersion" => "2.2.0", "maxVersion" => null, "mode" => DependencyMode::SOFT],
+        ["id" => Skills::ID, "minVersion" => "2.2.0", "maxVersion" => null, "mode" => DependencyMode::SOFT],
+        ["id" => Streaks::ID, "minVersion" => "2.2.0", "maxVersion" => null, "mode" => DependencyMode::SOFT],
+    ];
+    // NOTE: dependencies should be updated on code changes
+
+    const RESOURCES = [];
+
+
+    /*** ----------------------------------------------- ***/
+    /*** -------------------- Setup -------------------- ***/
+    /*** ----------------------------------------------- ***/
+
+    public function init()
+    {
+        $this->initTemplates();
+    }
+
+    public function disable()
+    {
+        $this->removeTemplates();
+    }
+}
