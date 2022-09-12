e9e155c3ee6bfa4e7de8f1042c980e9dcd8db4bb
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2022-09-12 19:44:58 +0100

    Changed project name

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/gamecourse-v2'),
+      dir: require('path').join(__dirname, './coverage/gamecourse'),
       subdir: '.',
       reporters: [
         { type: 'html' },
