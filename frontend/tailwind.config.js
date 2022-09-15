efd8630c3143788883c967a26c13579ae78b91d8
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2022-09-15 16:24:06 +0100

    Defined styles for light & dark themes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,48 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const lightThemeColors = require('daisyui/src/colors/themes')['[data-theme=light]'];
+const darkThemeColors = require('daisyui/src/colors/themes')['[data-theme=dark]'];
+
 module.exports = {
-  purge: {
-    content: [
-      './src/**/*.{html,ts,css,scss,sass,less,styl}',
-    ]
-  },
+  content: [
+    './src/**/*.{html,ts}',
+  ],
   plugins: [
     require('@tailwindcss/typography'),
+    // require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
     require('daisyui')
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['Inter', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        'info-focus': '#09AEf4',
+        'success-focus': '#2BC48C',
+        'warning-focus': '#E8A504',
+        'error-focus': '#ED4949',
+        'fenix': {
+          light: '#00a7ee',
+          DEFAULT: '#0095D5',
+          dark: '#0083bb'
+        },
+        'google': {
+          light: '#df584d',
+          DEFAULT: '#DB4437',
+          dark: '#d33426'
+        },
+        'facebook': {
+          light: '#5074be',
+          DEFAULT: '#4267B2',
+          dark: '#3b5c9f'
+        },
+        'linkedin': {
+          light: '#0088ce',
+          DEFAULT: '#0077B5',
+          dark: '#00669b'
+        }
+      },
       keyframes: {
         pulse: {
           '0%, 100%': { opacity: 0.9 },
@@ -22,35 +55,49 @@ module.exports = {
     themes: [
       {
         light: {
-          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
-          "primary": "#5e72e4",
-          "secondary": "#f4f5f7",
-          "accent": "#EA6FAC",
-          "neutral": "#172b4d",
-          "base-100": "#FFFFFF",
-          "info": "#38BDF8",
-          "success": "#34D399",
-          "warning": "#FBBF24",
-          "error": "#EF4444",
+          ...lightThemeColors,
+          'primary': '#5E72E4',
+          'primary-focus': '#485FE0',
+          'secondary': '#EA6FAC',
+          'secondary-focus': '#E7599F',
+          'accent': '#1EA896',
+          'accent-focus': '#1a9283',
+          'accent-content': lightThemeColors['base-100'],
+          'neutral': '#172B4D',
+          'neutral-focus': '#234174',
+          'base-200': '#F9FAFB',
+          'base-300': '#F4F5F7',
+          'info': '#38BFF8',
+          'success': '#36D399',
+          'warning': '#FBB50A',
+          'error': '#EF6060',
+          'error-content': '#420000',
         },
       },
       {
         dark: {
-          ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
-          "primary": "#5e72e4",
-          "secondary": "#f4f5f7",
-          "accent": "#EA6FAC",
-          "neutral": "#172b4d",
-          "base-100": "#2A303C",
-          "info": "#38BDF8",
-          "success": "#34D399",
-          "warning": "#FBBF24",
-          "error": "#EF4444",
+          ...darkThemeColors,
+          'primary': '#5E72E4',
+          'primary-focus': '#485FE0',
+          'secondary': '#EA6FAC',
+          'secondary-focus': '#E7599F',
+          'accent': '#1EA896',
+          'accent-focus': '#1a9283',
+          'accent-content': '#FFFFFF',
+          'neutral': '#E5E7EB',
+          'neutral-focus': '#C8CCD5',
+          'neutral-content': darkThemeColors['base-100'],
+          'base-content': '#BFC6D3',
+          'info': '#38BFF8',
+          'success': '#36D399',
+          'warning': '#FBBD23',
+          'error': '#EF6060',
+          'error-content': '#420000',
         },
       },
-      "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween",
-      "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk",
-      "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"
+      'cupcake', 'bumblebee', 'emerald', 'corporate', 'synthwave', 'retro', 'cyberpunk', 'valentine', 'halloween',
+      'garden', 'forest', 'aqua', 'lofi', 'pastel', 'fantasy', 'wireframe', 'black', 'luxury', 'dracula', 'cmyk',
+      'autumn', 'business', 'acid', 'lemonade', 'night', 'coffee', 'winter'
     ],
   },
 };
