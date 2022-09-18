529e4b89b7b4dd7121070f34936283019d603e88
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2022-09-18 03:04:57 +0100

    - Inputs: file - Minor fixes

--- frontend/src/app/_components/inputs/general/input-file/input-file.component.ts
+++ frontend/src/app/_components/inputs/general/input-file/input-file.component.ts
@@ -10,28 +10,32 @@ import {
   ViewChild
 } from '@angular/core';
 import {NgForm, NgModel} from "@angular/forms";
+import {AlertService, AlertType} from "../../../../_services/alert.service";
+import {InputGroupSize, InputSize } from '../../Settings';
 
 @Component({
   selector: 'app-input-file',
-  templateUrl: './input-file.component.html',
-  styleUrls: ['./input-file.component.scss']
+  templateUrl: './input-file.component.html'
 })
 export class InputFileComponent implements OnInit, AfterViewInit, OnChanges {
 
   // Essentials
-  @Input() id: string;                        // Unique id
-  @Input() form?: NgForm;                     // Form it's part of
-  @Input() accept?: string;                   // Types of files to accept
+  @Input() id: string;                                    // Unique ID
+  @Input() form: NgForm;                                  // Form it's part of
+  @Input() accept?: string;                               // Types of files to accept
+  @Input() multiple?: boolean;                            // Accept multiple files
+  @Input() camera?: boolean;                              // Accept camera input
 
   // Extras
-  @Input() label?: string;                    // Label prepend
-  @Input() multiple?: boolean;                // Accept multiple files
-  @Input() camera?: boolean;                  // Accept camera input
-  @Input() classList?: string;                // Classes to add
-  @Input() disabled?: boolean;                // Make it disabled
+  @Input() size?: 'xs' | 'sm' | 'md' | 'lg' = 'md';       // Size
+  @Input() color?: string;                                // Color
+  @Input() classList?: string;                            // Classes to add
+  @Input() disabled?: boolean;                            // Make it disabled
+
+  @Input() label?: string;                                // Top label text
 
   // Validity
-  @Input() required?: boolean;                // Make it required
+  @Input() required?: boolean;                            // Make it required
 
   // Errors
   @Input() requiredErrorMessage?: string;     // Message for required error
@@ -60,7 +64,66 @@ export class InputFileComponent implements OnInit, AfterViewInit, OnChanges {
 
   onFilesSelected(event) {
     this.files = event.target.files;
+
+    // Check if file format correct
+    if (this.accept.includes('image')) {
+      for (let i = 0; i < this.files.length; i++) {
+        const file = this.files.item(i);
+
+        if (!file.type.includes('image')) {
+          AlertService.showAlert(AlertType.ERROR, 'You can only upload images.');
+          this.files = null;
+          return;
+        }
+      }
+    }
+
     this.valueChange.emit(this.files);
+  }
+
+  getFiles(): File[] {
+    return Array.from(this.files);
+  }
+
+  getFileTypes(): string {
+    if (!this.accept) return 'Any file type';
+
+    const types = {
+      image: ['SVG', 'PNG', 'JPG', 'WEBP', 'GIF'],
+      video: ['MP4', 'MOV'],
+      audio: ['MP3', 'MID', 'WAV']
+    };
+
+    let formats: string[] = [];
+    if (this.accept.containsWord('image/*')) formats = formats.concat(types.image);
+    if (this.accept.containsWord('video/*')) formats = formats.concat(types.video);
+    if (this.accept.containsWord('audio/*')) formats = formats.concat(types.audio);
+
+    const singleTypes = this.accept.removeWord('image/*').removeWord('video/*').removeWord('audio/*');
+    const parts = singleTypes.split(',').map(part => part.trim().substring(1).toUpperCase()).filter(part => !!part);
+    formats = [...new Set(formats.concat(parts))]; // unique formats
+    return formats.length >= 2 ? (formats.slice(0, -1).join(', ') + ' or ' + formats.slice(-1)) : formats[0];
+  }
+
+  removeFile(index: number) {
+    // Remove file
+    let files = this.getFiles();
+    files.splice(index, 1);
+
+    // Create new filelist
+    const dt = new DataTransfer();
+    for (let file of files) {
+      dt.items.add(file);
+    }
+    this.files = dt.files;
+  }
+
+  get InputSize(): typeof InputSize {
+    return InputSize;
+  }
+
+  get InputGroupSize(): typeof InputGroupSize {
+    return InputGroupSize;
   }
 
 }
