dcac4b6e70c81d9ddf5381cada1c4e190c265d24
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2022-09-20 21:09:11 +0100

    Taking screenshot on error

--- frontend/src/app/_components/modals/error-modal/error-modal.component.ts
+++ frontend/src/app/_components/modals/error-modal/error-modal.component.ts
@@ -1,5 +1,8 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
+import html2canvas from "html2canvas";
+import * as moment from "moment";
+
 @Component({
   selector: 'app-error-modal',
   templateUrl: './error-modal.component.html'
@@ -9,9 +12,22 @@ export class ErrorModalComponent implements OnInit {
   @Input() error: { message: string, stack: string, full: string };
   @Output() onClose: EventEmitter<void> = new EventEmitter();
 
+  loading: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  takeScreenshot() {
+    this.loading = true;
+    html2canvas(document.body).then(canvas => {
+      const a = document.createElement('a');
+      a.download = moment().format('YYYY-MM-DD HH:mm:ss') + '.png';
+      a.href = canvas.toDataURL('image/png');
+      a.click();
+      this.loading = false;
+    });
   }
 
 }
