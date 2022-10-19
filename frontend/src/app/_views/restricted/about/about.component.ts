16939e3e9218db4684cf471e487755f9a63f6b7f
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2022-10-19 14:27:34 +0100

    UI test page

--- frontend/src/app/_views/restricted/about/about.component.ts
+++ frontend/src/app/_views/restricted/about/about.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ModalService } from 'src/app/_services/modal.service';
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'app-about',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  value;
+  value2;
+  @ViewChild('f', { static: false }) f: NgForm;
+
+  setSelected: Subject<any[]> = new Subject<any[]>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  test() {
+    ModalService.openModal('verification-modal')
+  }
+
+  onSubmit() {
+    if (this.f.form.valid) {
+      console.log('valid')
+
+    } else {
+      console.log('invalid')
+      console.log(this.f.errors)
+    }
+  }
 }
