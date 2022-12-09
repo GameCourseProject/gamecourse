911e59cf43e260bce2e202e17960609acadf2984
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2022-12-09 17:47:31 +0000

    - Allow negative values for rewards - Fix: getting user total reward by type of reward

--- api/modules/Awards/sql/create.sql
+++ api/modules/Awards/sql/create.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS award(
     description                 varchar(100) NOT NULL,
     type                        ENUM ('assignment', 'badge', 'bonus', 'exam', 'labs', 'post', 'presentation', 'quiz', 'skill', 'streak', 'tokens') NOT NULL,
     moduleInstance              int unsigned DEFAULT NULL,
-    reward                      int unsigned DEFAULT 0,
+    reward                      int DEFAULT 0,
     date                        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN key(user, course) REFERENCES course_user(id, course) ON DELETE CASCADE
