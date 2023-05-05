f72d7d89f47b8453fc357f298e563a2b2b3465e1
monicaalves1 <55749544+monicaalves1@users.noreply.github.com>
2023-05-05 16:04:35 +0100

    styling codemirror component

--- frontend/src/app/_components/inputs/code/input-code/input-code.component.ts
+++ frontend/src/app/_components/inputs/code/input-code/input-code.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@an
 import {EditorView, basicSetup} from "codemirror";
 import {EditorState, Compartment} from "@codemirror/state";
 import {syntaxTree} from "@codemirror/language";
+import {oneDark} from "@codemirror/theme-one-dark";
 // @ts-ignore
 import {autocompletion, CompletionContext, CompletionResult, CompletionSource} from "@codemirror/autocomplete";
 // @ts-ignore
@@ -23,6 +24,7 @@ export class InputCodeComponent implements OnInit, AfterViewInit {
   @Input() placeholder: string;               // Message to show by default
 
   // Extras
+  @Input() nrLines?: number = 10;             // Number of lines already added to the editor. Default = 10 lines
   @Input() title?: string;                    // Textarea title
   @Input() classList?: string;                // Classes to add
   @Input() disabled?: boolean;                // Make it disabled
@@ -61,6 +63,7 @@ export class InputCodeComponent implements OnInit, AfterViewInit {
     let state = EditorState.create({
       extensions: [
         basicSetup,
+        oneDark,
         tabSize.of(EditorState.tabSize.of(8)),
         language.of(python()),
         autocompletion({override: [completePy]})
@@ -70,7 +73,7 @@ export class InputCodeComponent implements OnInit, AfterViewInit {
     const context = new CompletionContext(state, 0, true);
 
     let editor = new EditorView({
-      doc: "// Type a 'p'\n",
+      doc: "# " + this.placeholder + "\n",
       state,
       parent: element
     });
@@ -102,112 +105,23 @@ export class InputCodeComponent implements OnInit, AfterViewInit {
       }
     }
 
-    //const pyCompletions = pythonLanguage.data.of({autocomplete: completePy});
-    /*myCompletions(context, this.options);
 
-    function myCompletions(context: CompletionContext, options: any[]) {
-      console.log("hey");
-      let before = context.matchBefore(/\w+/)
-      // If completion wasn't explicitly started and there
-      // is no word before the cursor, don't open completions.
-      if (!context.explicit && !before) return null
-
-      console.log(options);
-      return {
-        from: before ? before.from : context.pos,
-        options: options,
-        validFor: /^\w*$/
+    // Set number of lines initialized
+    updateToMinNumberOfLines(editor, this.nrLines);
+    function updateToMinNumberOfLines(editor, minNumOfLines) {
+      const currentNumOfLines = editor.state.doc.lines;
+      const currentStr = editor.state.doc.toString();
+      if (currentNumOfLines >= minNumOfLines) {
+        return;
       }
-    }*/
-
-
-  }
-
-
-  /*"use strict";
-
-  this.mode = "python";
-  CodeMirror(document.getElementById("my-div"), {
-    value: "",
-    mode: "python",
-    tabSize: 5,
-    lineNumbers: true,
-    firstLineNumber: 50,
-    extraKeys: {"Ctrl-Space": "autocomplete"},
-    showHint: true
-  });
-
-  let WORD = /[\w$]+/;
-  let RANGE = 500;
-  let EXTRAWORDS = ['amazing', 'extra', 'yeah', 'toto'];
-
-  CodeMirror.registerHelper('hint', "anyword", function (editor, options) {
-    let word = options && options.word || WORD;
-    let range = options && options.range || RANGE;
-    let extraWords = options && options.extrawords || EXTRAWORDS;
-    let cur = editor.getCursor(), curLine = editor.getLine(cur.line);
-
-    let end = cur.ch, start = end;
-    while (start && word.test(curLine.charAt(start - 1))) --start;
-    let curWord = start != end && curLine.slice(start, end);
-
-    let list = options && options.list || [], seen = {};
-    let re = new RegExp(word.source, "g");
-
-    for (let dir = -1; dir <= 1; dir += 2) {
-      let line = cur.line,
-        endLine = Math.min(Math.max(line + dir * range, editor.firstLine()), editor.lastLine()) + dir;
-
-      for (; line != endLine; line += dir) {
-        let text = editor.getLine(line), m;
-        while (m = re.exec(text)) {
-          if (line == cur.line && m[0] === curWord) continue;
-          if ((!curWord || m[0].lastIndexOf(curWord, 0) == 0) && !Object.prototype.hasOwnProperty.call(seen, m[0]))
-          {
-            seen[m[0]] = true;
-            list.push(m[0]);
-          }
-        }
-      }
+      const lines = minNumOfLines - currentNumOfLines;
+      const appendLines = "\n".repeat(lines);
+      editor.dispatch({
+        changes: {from: currentStr.length, insert: appendLines}
+      })
     }
-    list.push(...(extraWords.filter(el => el.startsWith(curWord || ''))));
-    return {list: list, from: CodeMirror.Pos(cur.line, start), to: CodeMirror.Pos(cur.line, end)};
-  });
 
-
-  const that = this;
-  console.log(CodeMirror.hint);
-  CodeMirror.commands.autocomplete = function (cm) {
-    cm.showHint({hint: CodeMirror.hint[that.mode]})
-  };
-}
-  /*if (this.codemirror) return;
-
-  if (!this.options) {
-    this.options = {
-      lineNumbers: true,
-      styleActiveLine: true,
-      autohint: true,
-      lineWrapping: true,
-      theme: "mdn-like"
-    };
   }
-
-  this.options['mode'] = this.mode;
-  this.options['value'] = !this.init?.isEmpty() ? this.init : null;
-
-  const textarea = $('#' + this.id)[0] as HTMLTextAreaElement;
-  this.codemirror = CodeMirror.fromTextArea(textarea, this.options);
-
-  const that = this;
-  this.codemirror.on("keyup", function (cm, event) {
-    cm.showHint(CodeMirror.hint[that.mode]);
-  });
-
-  this.codemirror.on("change", function (cm, event) {
-    that.valueChange.emit(that.codemirror.getValue());
-  });
-}*/
 
 
 }
