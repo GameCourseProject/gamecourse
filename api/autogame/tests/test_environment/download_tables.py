23635a17514d7c4e5613af5bd28a24230bc4cfd0
monicaalves1 <55749544+monicaalves1@users.noreply.github.com>
2023-07-09 11:35:53 +0100

    fix null fields in import/export tables for testing environment

--- api/autogame/tests/test_environment/download_tables.py
+++ api/autogame/tests/test_environment/download_tables.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from io import StringIO
 
-import os, sys, json, csv
+import os, sys, json, csv, glob
 import mysql.connector
 
 # bytearray(b\'text\') -> transformed into 'text' (as string)
@@ -12,36 +12,54 @@ def decode_item(item):
         decoded_string = byte_array.decode()
         return decode_item(decoded_string)
 
-    #bytearray(b'Profile')
     elif isinstance(item, bytearray):
         return decode_item(item.decode())
 
     else:
         return item
 
-# CLI prompt: python3 download_tables.py [dbHost] [dbName] [dbUser] [dbPass]
+# CLI prompt: python3 download_tables.py [dbHost] [dbName] [dbUser] [dbPass] [table_name]
 if __name__ == "__main__":
     """
         This script downloads information from the db's tables so later the DB can be re-instantiated for testing.
+        Optional to pass a specific table_name to download
     """
 
-    if len(sys.argv) == 5:
+    if len(sys.argv) >= 5:
 
         output_directory = "./tables_files/"
 
         cnx = mysql.connector.connect(user=sys.argv[3], password=sys.argv[4], host=sys.argv[1], database=sys.argv[2])
         cursor = cnx.cursor(prepared=True)
 
-        cursor.execute("SHOW TABLES")
+        # Delete all files before starting download
+        file_list = glob.glob(os.path.join(output_directory, '*'))
+
+        # Iterate over the files and delete 1 by 1
+        for file_path in file_list:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
+        if len(sys.argv) == 6:
+            query = "SELECT * FROM " + sys.argv[5] + ";"
+
+        else:
+            query = "SHOW TABLES"
+
+        cursor.execute(query)
         tables = cursor.fetchall()
 
         for table in tables:
-            table_name = table[0]
-            file_name = f"{table_name.decode()}.csv"
+            if len(sys.argv) == 6:
+                table_name = sys.argv[5]
+            else:
+                table_name = table[0].decode()
+
+            file_name = f"{table_name}.csv"
             file_path = output_directory + file_name
 
             # Retrieve data from the table
-            query = "SELECT * FROM " + table_name.decode() + ";"
+            query = "SELECT * FROM " + table_name + ";"
             cursor.execute(query)
             data = cursor.fetchall()
 
@@ -55,7 +73,13 @@ if __name__ == "__main__":
                         decoded_row.append(decode_item(item))
                     csv_writer.writerow(decoded_row)
 
-            print(table_name.decode() + "Exported successfully")
+            print(table_name + " - EXPORTED SUCCESSFULLY")
+
+            if len(sys.argv) == 6:
+                break
+
+        print("---------------------------------")
+        print(str(len(tables)) + " file(s) exported.")
 
         cursor.close()
         cnx.close()
--- api/autogame/tests/test_environment/upload_tables.py
+++ api/autogame/tests/test_environment/upload_tables.py
@@ -5,24 +5,30 @@ from io import StringIO
 import os, sys, json, csv
 import mysql.connector
 
-# CLI prompt: python3 download_tables.py [dbHost] [dbName] [dbUser] [dbPass]
+# CLI prompt: python3 download_tables.py [dbHost] [dbName] [dbUser] [dbPass] [table_name]
 if __name__ == "__main__":
     """
         This script uploads the information from cvs files inside the directory 
         tables_files into the DB. It acts as a re-instantiation of the DB for the testing environment.
+        Optional to pass a specific table_name to upload
     """
 
-    if len(sys.argv) == 5:
+    if len(sys.argv) >= 5:
 
         input_directory = "./tables_files/"
 
         cnx = mysql.connector.connect(user=sys.argv[3], password=sys.argv[4], host=sys.argv[1], database=sys.argv[2])
         cursor = cnx.cursor(prepared=True)
 
+        if len(sys.argv) == 6:
+            list = [sys.argv[5]+ ".csv"]
+        else:
+            list = os.listdir(input_directory)
+
         # Disable foreign key checks temporarily
         cursor.execute("SET FOREIGN_KEY_CHECKS = 0")
 
-        for filename in os.listdir(input_directory):
+        for filename in list:
             if filename.endswith(".csv"):
                 file_path = os.path.join(input_directory, filename)
                 table_name = os.path.splitext(filename)[0]
@@ -38,10 +44,25 @@ if __name__ == "__main__":
                 if len(data) > 0:
                     # Construct the SQL INSERT statement and execute it to insert the data into the table
                     query = "INSERT INTO " + table_name + f" VALUES ({', ' . join(['%s'] * len(data[0]))});"
-                    cursor.executemany(query, data)
+
+                    parsed_data = []
+                    for column in data:
+                        parsed_values = []
+                        for value in column:
+                            if value == '':
+                                parsed_values += [None]
+                            else:
+                                parsed_values += [value]
+
+                        parsed_data += [parsed_values]
+
+                    cursor.executemany(query, parsed_data)
                     cnx.commit()
 
-            print(os.path.splitext(filename)[0] + "Imported successfully")
+                print(table_name + " - IMPORTED SUCCESSFULLY")
+
+        print("---------------------------------")
+        print(str(len(list)) + " file(s) imported.")
 
         # Enable foreign key checks again
         cursor.execute("SET FOREIGN_KEY_CHECKS = 1")
