351ca4dda51071df3be2020a8b0b08b476de3035
mara-alves <maraalves2001@gmail.com>
2024-02-11 20:52:56 +0000

    Fix path on notifications script

--- api/models/GameCourse/NotificationSystem/scripts/NotificationsScript.php
+++ api/models/GameCourse/NotificationSystem/scripts/NotificationsScript.php
@@ -7,14 +7,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-use GameCourse\Core\Core;
 use GameCourse\Course\Course;
 use GameCourse\Module\Module;
 use GameCourse\NotificationSystem\Notification;
-use Utils\CronJob;
-use Utils\Utils;
 
-require __DIR__ . "/../../../inc/bootstrap.php";
+require __DIR__ . "/../../../../inc/bootstrap.php";
 
 $courseId = $argv[1];
 $course = Course::getCourseById($courseId);
@@ -29,12 +26,5 @@ foreach ($students as $student) {
 
     if ($message) {
         Notification::addNotification($courseId, $student["id"], $message);
-    }
-}
-
-if (!$error) {
-    if ($timeLeft == 0) {
-        $script = "models/GameCourse/NotificationSystem/scripts/NotificationsScript.php";
-        CronJob::removeCronJob($script, $courseId, $moduleId);
     }
 }
