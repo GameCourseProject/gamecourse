87a588707b16b848d4e72b89bef1e6ea24bc0b78
mara-alves <maraalves2001@gmail.com>
2024-02-29 18:43:48 +0000

    Upgrade slim-select

--- frontend/src/app/_views/restricted/courses/course/settings/modules/config/personalized-config/skills/skills.component.ts
+++ frontend/src/app/_views/restricted/courses/course/settings/modules/config/personalized-config/skills/skills.component.ts
@@ -313,7 +313,7 @@ export class SkillsComponent implements OnInit {
 
       if (action === 'value changed') {
         this.getSkillTreeInfo(this.skillTreeInView.id).loading.skills = true;
-        
+
         if (col === 5) skillToActOn.isCollab = value;
         else if (col === 6) skillToActOn.isExtra = value;
         else if (col === 7) skillToActOn.isActive = value;
@@ -652,9 +652,9 @@ export class SkillsComponent implements OnInit {
     });
   }
 
-  getDependencyOptions(): ({value: string, text: string, innerHTML: string} | {label: string, options: {value: string, text: string}[]})[] {
+  getDependencyOptions(): ({value: string, text: string, html: string} | {label: string, options: {value: string, text: string}[]})[] {
     const skillTreeInfo = this.getSkillTreeInfo(this.skillTreeInView.id);
-    const options: ({value: string, text: string, innerHTML: string} | {label: string, options: {value: string, text: string}[]})[] = [];
+    const options: ({value: string, text: string, html: string} | {label: string, options: {value: string, text: string}[]})[] = [];
 
     const skillTier = skillTreeInfo.tiers.find(tier => tier.id === parseInt(this.skillToManage.tierID.substring(3)));
     skillTreeInfo.tiers.forEach(tier => {
@@ -668,7 +668,7 @@ export class SkillsComponent implements OnInit {
     });
 
     if (addWildcardOption(skillTreeInfo.tiers, skillTreeInfo.skills))
-      options.unshift({value: 'id-0', text: Tier.WILDCARD, innerHTML: '<span class="text-secondary">' + Tier.WILDCARD + '</span>'})
+      options.unshift({value: 'id-0', text: Tier.WILDCARD, html: '<span class="text-secondary">' + Tier.WILDCARD + '</span>'})
 
     return options;
 
