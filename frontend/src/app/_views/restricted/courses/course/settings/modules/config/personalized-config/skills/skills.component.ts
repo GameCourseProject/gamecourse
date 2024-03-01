634c47b59b5b431b6b671cd334a5527414b92fd1
mara-alves <maraalves2001@gmail.com>
2024-03-01 18:56:15 +0000

    Change exponential formula again

--- frontend/src/app/_views/restricted/courses/course/settings/modules/config/personalized-config/skills/skills.component.ts
+++ frontend/src/app/_views/restricted/courses/course/settings/modules/config/personalized-config/skills/skills.component.ts
@@ -203,9 +203,9 @@ export class SkillsComponent implements OnInit {
       if (this.VCEnabled) {
         row.splice(3, 0, {
           type: TableDataType.TEXT, content: {
-            text: tier.costType.capitalize() + ': ' + tier.cost +
-              (tier.costType === 'incremental' ? (' + ' + tier.increment + ' x #attempts (rating >= ' + tier.minRating + ')')
-                : tier.costType === 'exponential' ? (' + ' + tier.increment + ' x 2 ^ (#attempts (rating >= ' + tier.minRating + ') - 1)')
+            text: tier.costType.capitalize() +
+              (tier.costType === 'incremental' ? (': ' + tier.cost + ' + ' + tier.increment + ' x #attempts (rating >= ' + tier.minRating + ')')
+                : tier.costType === 'exponential' ? (': 1st attempt → ' + tier.cost + ', others → ' + tier.increment + ' x 2 ^ (#attempts - 1)')
                 : '')
           }
         });
@@ -313,7 +313,7 @@ export class SkillsComponent implements OnInit {
 
       if (action === 'value changed') {
         this.getSkillTreeInfo(this.skillTreeInView.id).loading.skills = true;
-        
+
         if (col === 5) skillToActOn.isCollab = value;
         else if (col === 6) skillToActOn.isExtra = value;
         else if (col === 7) skillToActOn.isActive = value;
