6d747cccc7b98ab28c25bd09862730a57d88e149
mara-alves <maraalves2001@gmail.com>
2024-05-26 17:23:51 +0100

    Fix backspace/delete after moving from view editor to another page

--- frontend/src/app/_components/building-blocks/any/any.component.ts
+++ frontend/src/app/_components/building-blocks/any/any.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {ChangeDetectorRef, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import * as _ from "lodash";
 
 import {View, ViewMode} from "../../../_domain/views/view";
@@ -34,7 +34,7 @@ import {AlertService, AlertType} from "../../../_services/alert.service";
   templateUrl: './any.component.html',
   styleUrls: ['./any.component.scss']
 })
-export class BBAnyComponent implements OnInit {
+export class BBAnyComponent implements OnInit, OnDestroy {
 
   @Input() view: View;
   @Input() isExistingRoot: boolean = false;
@@ -67,14 +67,21 @@ export class BBAnyComponent implements OnInit {
     });
 
     if (this.view.mode === ViewMode.EDIT) {
-      addEventListener('keydown', (event: KeyboardEvent) => {
-        if (this.isSelected() && !ModalService.isAnyOpen()) {
-          if (event.key === 'Delete' || event.key === 'Backspace') {
-            event.preventDefault();
-            ModalService.openModal('component-delete-' + this.view.id);
-          }
-        }
-      })
+      this.handleKeyDown = this.handleKeyDown.bind(this);
+      addEventListener('keydown', this.handleKeyDown);
+    }
+  }
+
+  ngOnDestroy() {
+    removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(event: KeyboardEvent) {
+    if (this.isSelected() && !ModalService.isAnyOpen()) {
+      if (event.key === 'Delete' || event.key === 'Backspace') {
+        event.preventDefault();
+        ModalService.openModal('component-delete-' + this.view.id);
+      }
     }
   }
 
