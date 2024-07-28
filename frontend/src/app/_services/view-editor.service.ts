78c87b374931c427230a8be9af8388aa13e25ea9
mara-alves <maraalves2001@gmail.com>
2024-07-28 15:56:53 +0100

    Simplify code for delete on aspects in root

--- frontend/src/app/_services/view-editor.service.ts
+++ frontend/src/app/_services/view-editor.service.ts
@@ -5,13 +5,11 @@ import * as _ from "lodash"
 import {
   addToGroupedChildren,
   addVariantToGroupedChildren,
-  getFakeId,
   groupedChildren,
   viewsDeleted
 } from '../_domain/views/build-view-tree/build-view-tree';
 import {Subject} from 'rxjs';
 import {ViewTable} from '../_domain/views/view-types/view-table';
-import {buildView} from "../_domain/views/build-view/build-view";
 import {ViewBlock} from "../_domain/views/view-types/view-block";
 import {ViewCollapse} from "../_domain/views/view-types/view-collapse";
 
@@ -270,58 +268,29 @@ export class ViewEditorService {
       newBlock.switchMode(ViewMode.EDIT);
       addToGroupedChildren(newBlock, null);
 
-      // the parent also has a parent already -> just make an alternative block then
       if (item.parent.parent) {
         newBlock.parent = item.parent.parent;
         addVariantToGroupedChildren(item.parent.parent.id, item.parent.id, newBlock.id);
+      }
+      else {
+        newBlock.parent = null;
+        addToGroupedChildren(newBlock, null);
+      }
 
-        for (let el of lowerInHierarchy) {
-          let view = el.view.findView(item.parent.id);
+      lowerInHierarchy.forEach((el) => {
+        let view = el.view.findView(item.parent.id);
+        if (!view.parent) {
+          this.viewsByAspect = this.viewsByAspect.map(e => {
+            if (_.isEqual(e, el)) {
+              return {aspect: e.aspect, view: _.cloneDeep(newBlock)}
+            } else return e;
+          })
+          this.selectedChange.next(this.getSelectedView());
+        }
+        else {
           view.parent.replaceView(view.id, _.cloneDeep(newBlock));
         }
-      }
-      // make a new block with the original content, so the parent has the two alternatives (original and new) as child
-      // keeping the root the same
-      else {
-        const baseBlock = buildView({
-          id: getFakeId(),
-          viewRoot: null,
-          aspect: item.parent.aspect,
-          type: "block",
-          class: ""
-        });
-        baseBlock.switchMode(ViewMode.EDIT);
-        newBlock.classList = "";
-
-        if (item.parent instanceof ViewBlock && baseBlock instanceof ViewBlock) {
-          for (let el of lowerInHierarchy) {
-            const found = el.view.findView(item.parent.id);
-            if (found instanceof ViewBlock) found.children = [_.cloneDeep(newBlock)];
-          }
-
-          for (let el of this.viewsByAspect.filter(e => lowerInHierarchy.indexOf(e) == -1)) {
-            const found = el.view.findView(item.parent.id);
-            if (found instanceof ViewBlock) {
-              const otherBlock = _.cloneDeep(baseBlock);
-              otherBlock.parent = item.parent;
-              otherBlock.children = _.cloneDeep(found.children);
-
-              found.children = [otherBlock];
-
-              for (let el of otherBlock.children) {
-                el.parent = otherBlock
-              }
-            }
-          }
-
-          const oldChildren = groupedChildren.get(item.parent.id);
-          groupedChildren.set(baseBlock.id, oldChildren);
-          groupedChildren.set(item.parent.id, [[baseBlock.id]]);
-          addVariantToGroupedChildren(item.parent.id, baseBlock.id, newBlock.id);
-
-          newBlock.parent = item.parent;
-        }
-      }
+      })
 
     }
     // item has no parent at all -> it's the root we are deleting -> only gets here if it's a new page -> it's fine! just make it null
