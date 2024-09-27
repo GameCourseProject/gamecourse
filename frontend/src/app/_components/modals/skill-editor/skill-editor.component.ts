a3c5debb5ab2d34b6918627a9a27c9a469b3f9d5
mara-alves <maraalves2001@gmail.com>
2024-09-27 16:35:24 +0100

    Let skill be edited in path

--- frontend/src/app/_components/modals/skill-editor/skill-editor.component.ts
+++ frontend/src/app/_components/modals/skill-editor/skill-editor.component.ts
@@ -1,9 +1,14 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from "@angular/core";
 import {
   SkillManageData
 } from "../../../_views/restricted/courses/course/settings/modules/config/personalized-config/skills/skills.component";
 import {Skill} from "../../../_domain/modules/config/personalized-config/skills/skill";
 import * as _ from 'lodash';
+import {clearEmptyValues} from "../../../_utils/misc/misc";
+import {ModalService} from "../../../_services/modal.service";
+import {AlertService, AlertType} from "../../../_services/alert.service";
+import {ApiHttpService} from "../../../_services/api/api-http.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-skill-editor',
@@ -11,17 +16,20 @@ import * as _ from 'lodash';
 })
 export class SkillEditorComponent implements OnInit {
 
+  @Input() courseID: number;
   @Input() courseFolder: string;
   @Input() skill: Skill;
-  @Input() loading: boolean = false;
 
+  @Output() onClose: EventEmitter<void> = new EventEmitter();
+
+  @ViewChild('fSkill', { static: false }) fSkill: NgForm;
+
+  loading: boolean = false;
   skillPageMode: 'editor' | 'preview' = 'editor';
   skillToManage: SkillManageData;
-  onSubmit: () => void;
-  reset: () => void;
-
 
   constructor(
+    private api: ApiHttpService,
   ) { }
 
   ngOnInit(): void {
@@ -48,7 +56,29 @@ export class SkillEditorComponent implements OnInit {
   }
 
   /*** --------------------------------------------- ***/
-  /*** ------------------ Helpers ------------------ ***/
+  /*** ------------------ Actions ------------------ ***/
   /*** --------------------------------------------- ***/
+  async onSubmit(): Promise<void> {
+    if (this.fSkill.valid) {
+      this.loading = true;
+
+      const skillToEdit = await this.api.getSkillById(this.skillToManage.id).toPromise();
+      skillToEdit.name = this.skillToManage.name;
+      skillToEdit.color = this.skillToManage.color;
+      skillToEdit.page = this.skillToManage.page;
+
+      await this.api.editSkill(this.courseID, clearEmptyValues(skillToEdit)).toPromise();
+      ModalService.closeModal('skill-manage');
+      AlertService.showAlert(AlertType.SUCCESS, 'Skill \'' + this.skillToManage.name + '\' edited');
+      this.reset();
+
+      this.loading = false;
+
+    } else AlertService.showAlert(AlertType.ERROR, 'Invalid form');
+  }
+
+  reset() {
+    this.onClose.emit();
+  }
 
 }
